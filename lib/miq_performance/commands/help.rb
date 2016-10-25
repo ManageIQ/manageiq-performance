80dee3c8a68d59f08499eb1811ec545a2c780a5d
Nick LaMuro <nicklamuro@gmail.com>
2016-10-25 15:59:10 -0500

    Handle commands that don't load in help sub cmd
    There will some commands that are provided by miqperf that require
    optional dependant gems, so allow `miqperf` to function in both
    situations:  with and without the dependency.
    
    Instead of displaying help docs for them regardless if sub command will
    actually work, try to load them, but if a NameError is raised (the
    command files themselves should handle not defining the class if the
    dependency doesn't exists), then rescue and continue on.

--- lib/miq_performance/commands/help.rb
+++ lib/miq_performance/commands/help.rb
@@ -7,12 +7,20 @@ module MiqPerformance
         command_files = Dir["#{File.dirname __FILE__}/*"].sort
         ljust_length  = command_files.map {|f| File.basename(f).length}.max
         command_files.each do |command_file|
-          require command_file
+          begin
+            require command_file
 
-          cmd   = File.basename(command_file, ".*")
-          klass = MiqPerformance::Commands.const_get(cmd.capitalize)
+            cmd   = File.basename(command_file, ".*")
+            klass = MiqPerformance::Commands.const_get(cmd.capitalize)
 
-          help_text << "    #{cmd.ljust ljust_length} #{klass.help_text}"
+            help_text << "    #{cmd.ljust ljust_length} #{klass.help_text}"
+          rescue NameError
+            # The class isn't defined, so move on
+            #
+            # This can be caused by a dependent gem not being installed, so
+            # allow the rest of the commands to still function that don't
+            # require that dependency.
+          end
         end
 
         puts help_text.join("\n")
