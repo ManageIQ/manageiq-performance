9e7c85e92c71d0ca1955b0e825c6bcc62cbc8e06
Nick LaMuro <nicklamuro@gmail.com>
2021-01-15 15:45:28 -0600

    [middleware_storage/file_spec.rb] Refactor #record
    Moves common vars into let statements, and makes use of context
    statements to be able to share `data_writer` between all specs.

--- spec/manageiq_performance/middleware_storage/file_spec.rb
+++ spec/manageiq_performance/middleware_storage/file_spec.rb
@@ -29,29 +29,30 @@ describe ManageIQPerformance::MiddlewareStorage::File do
   end
 
   describe "#record" do
-    let(:filestore) { described_class.new }
-    let(:env)       { {"REQUEST_PATH" => "/foo/bar"} }
+    let(:filestore)         { described_class.new }
+    let(:env)               { {"REQUEST_PATH" => "/foo/bar"} }
+    let(:data_writer)       { Proc.new { data } }
+    let(:suite_dir)         { "#{proj_dir}/tmp/manageiq_performance/run_1234567" }
+    let(:expected_filename) { "foo%bar/request_1234567000000.info" }
 
     after(:each)    { FileUtils.rm_rf "#{proj_dir}/tmp/manageiq_performance" }
 
-    it "writes a report to the given filename" do
-      data = "my middleware dataz"
-      data_writer = Proc.new { data }
-      suite_dir = "#{proj_dir}/tmp/manageiq_performance/run_1234567"
-      expected_filename = "foo%bar/request_1234567000000.info"
+    context "for string data" do
+      let(:data) { "my middleware dataz" }
 
-      filestore.record env, :info, nil, data_writer
-      expect(File.read "#{suite_dir}/#{expected_filename}").to eq data
+      it "writes a basic report to the given filename" do
+        filestore.record env, :info, nil, data_writer
+        expect(File.read "#{suite_dir}/#{expected_filename}").to eq data
+      end
     end
 
-    it "writes a report as yml if given a hash" do
-      data = {"foo" => "my middleware dataz"}
-      data_writer = Proc.new { data }
-      suite_dir = "#{proj_dir}/tmp/manageiq_performance/run_1234567"
-      expected_filename = "foo%bar/request_1234567000000.info"
+    context "for hash data" do
+      let(:data) { {"foo" => "my middleware dataz"} }
 
-      filestore.record env, :info, nil, data_writer
-      expect(File.read "#{suite_dir}/#{expected_filename}").to eq data.to_yaml
+      it "writes a report as yml if given a hash" do
+        filestore.record env, :info, nil, data_writer
+        expect(File.read "#{suite_dir}/#{expected_filename}").to eq data.to_yaml
+      end
     end
   end
 
